888873eef1b38ebf039bf86e3dad9d089c63728b
Bablu  Kumar <bablu.kumar@webnyxa.tech>
2023-03-22 18:52:12 +0530

    admin panel details

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
     path('job_booking_list/', views.job_booking_list, name='job_booking_list'),
     path('job_details/',JobDetailsAdmin.as_view()),
+    path('job_details_admin/',JobDetailsAdminPanel.as_view()),
 
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -192,3 +192,106 @@ class JobDetailsAdmin(APIView):
                     })    
 
         return Response({'data': data})
+
+
+
+class JobDetailsAdminPanel(APIView):
+    permission_classes=[AllowAny,]
+    def get(self,request,format=None):
+        data=request.GET.get('id')
+        data_list=[]
+        if JobSahayak.objects.filter(id=data,status='Pending').exists():
+            details1=JobSahayak.objects.get(id=data)
+            data_list.append({
+                'grahak_name':details1.grahak.profile.name,
+                'grahak_gender':details1.grahak.profile.name,
+                'grahak_phone':details1.grahak.mobile_no,
+                'grahak_mohalla':details1.grahak.profile.mohalla,
+                'grahak_village':details1.grahak.profile.village,
+                'grahak_state':details1.grahak.profile.state,
+                'grahak_district':details1.grahak.profile.district,
+                'status':details1.status,
+                'desc':details1.description,
+                 
+                'heading':"",
+                'name':"",
+                'gender':"",
+                'phone':"",
+                'mohalla':"",
+                'village':"",
+                'state':"",
+                'district':"",
+
+            })
+        elif JobMachine.objects.filter(id=data,status='Pending').exists():
+            details2=JobMachine.objects.get(id=data)
+            data_list.append({
+                'grahak_name':details2.grahak.profile.name,
+                'grahak_gender':details2.grahak.profile.gender,
+                'grahak_phone':details2.grahak.mobile_no,
+                'grahak_mohalla':details2.grahak.profile.mohalla,
+                'grahak_village':details2.grahak.profile.village,
+                'grahak_state':details2.grahak.profile.state,
+                'grahak_district':details2.grahak.profile.district,
+                'status':details2.status,
+                'desc':details2.description,
+                
+                'heading':"",
+                'name':"",
+                'gender':"",
+                'phone':"",
+                'mohalla':"",
+                'village':"",
+                'state':"",
+                'district':"",
+
+            })
+        elif JobBooking.objects.filter(id=data ,status='Accepted').exists():
+            details3=JobBooking.objects.get(id=data)
+            if details3.jobsahayak:
+                data_list.append({
+                    'grahak_name':details3.jobsahayak.grahak.profile.name,
+                    'grahak_gender':details3.jobsahayak.grahak.profile.gender,
+                    'grahak_phone':details3.jobsahayak.grahak.mobile_no,
+                    'grahak_mohalla':details3.jobsahayak.grahak.profile.mohalla,
+                    'grahak_village':details3.jobsahayak.grahak.profile.village,
+                    'grahak_state':details3.jobsahayak.grahak.profile.state,
+                    'grahak_district':details3.jobsahayak.grahak.profile.district,
+                    'desc':details3.jobsahayak.description,
+                    'status':details3.status,
+                    
+                    'heading':"Sahayak Details",
+                    'name':details3.booking_user.profile.name,
+                    'gender':details3.booking_user.profile.gender,
+                    'phone':details3.booking_user.mobile_no,
+                    'mohalla':details3.booking_user.profile.mohalla,
+                    'village':details3.booking_user.profile.village,
+                    'state':details3.booking_user.profile.state,
+                    'district':details3.booking_user.profile.district,
+
+                })
+            else:
+                data_list.append({
+                    'grahak_name':details3.jobmachine.grahak.profile.name,
+                    'grahak_gender':details3.jobmachine.grahak.profile.gender,
+                    'grahak_phone':details3.jobmachine.grahak.mobile_no,
+                    'grahak_mohalla':details3.jobmachine.grahak.profile.mohalla,
+                    'grahak_village':details3.jobmachine.grahak.profile.village,
+                    'grahak_state':details3.jobmachine.grahak.profile.state,
+                    'grahak_district':details3.jobmachine.grahak.profile.district,
+                    'desc':details3.jobmachine.description,
+                    'status':details3.status,
+                    
+                    'heading':"Machine Malik Details",
+                    'name':details3.booking_user.profile.name,
+                    'gender':details3.booking_user.profile.gender,
+                    'phone':details3.booking_user.mobile_no,
+                    'mohalla':details3.booking_user.profile.mohalla,
+                    'village':details3.booking_user.profile.village,
+                    'state':details3.booking_user.profile.state,
+                    'district':details3.booking_user.profile.district,
+                })  
+
+        else:
+            return Response({'Job not exists !'})         
+        return Response(data_list)    
