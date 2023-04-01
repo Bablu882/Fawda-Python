336c710fb8e3063e66fe43fe3006693e6b900279
Bablu  Kumar <bablu.kumar@webnyxa.tech>
2023-04-01 14:09:10 +0530

    addedd test payments

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Payment(models.Model):
+    booking_id = models.CharField(max_length=100)
+    amount = models.CharField(max_length=100,null=True,blank=True)
+    payment_id = models.CharField(max_length=100)
+    payment_status = models.CharField(max_length=100)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    beneficiary_name=models.CharField(max_length=100,null=True,blank=True)
+
+    def __str__(self):
+        return f'{self.booking_id} - {self.amount} - {self.payment_status}'
--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,100 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+# import requests
+from .models import Payment
+from booking.models import JobBooking
 
-# Create your views here.
+# class PaymentAPIView(APIView):
+#     def post(self, request):
+#         if request.user.user_type =='Grahak':
+#             booking_id = request.POST.get('booking_id')
+#             amount = request.POST.get('amount')
+            
+#             # Get the client's UPI details
+#             upi_id = 'client@upi'  # Replace with the actual UPI ID
+#             beneficiary_name = 'Acme Corporation'  # Replace with the actual beneficiary name
+            
+#             # Make an API call to HDFC UPI's payment API endpoint with the client's UPI details
+#             response = requests.post('https://api.hdfcbank.com/upi/payments', data={
+#                 'booking_id': booking_id,
+#                 'amount': str(amount),
+#                 'upi_id': upi_id,
+#                 'beneficiary_name': beneficiary_name,
+#                 # Add any other required parameters for HDFC UPI payment API
+#             })
+
+#             # If payment is successful, save payment details to Payment table
+#             if response.status_code == 200 and response.json().get('status') == 'success':
+#                 payment_id = response.json().get('payment_id')
+#                 payment_status = response.json().get('payment_status')
+
+#                 payment = Payment.objects.create(
+#                     booking_id=booking_id,
+#                     amount=str(amount),
+#                     payment_id=payment_id,
+#                     payment_status=payment_status,
+#                     beneficiary_name=beneficiary_name
+#                 )
+#                 job=JobBooking.objects.get(pk=booking_id)
+#                 job.status= 'Booked'
+#                 job.save()
+#                 if job.jobsahayak:
+#                     if job.jobsahayak.job_type == 'individuals_sahayak':
+#                         if job.jobsahayak.count_male == '0' and job.jobsahayak.count_female == '0':
+#                             job.jobsahayak.status = 'Booked'
+#                             job.save()
+#                     else:
+#                         job.jobsahayak.status = 'Booked'
+#                         job.save()        
+#                 else:
+#                     job.jobmachine.status='Booked'        
+#                     job.save()
+#                 return Response({'status': 'success'})
+#             else:
+#                 # If payment fails, return an error message to the mobile app
+#                 return Response({'status': 'error', 'message': 'Payment failed'})
+
+
+
+class TestPaymentAPIView(APIView):
+    def post(self, request):
+        if request.user.user_type == 'Grahak':
+            booking_id = request.data.get('booking_id')
+            amount = request.data.get('amount')
+            upi_id = 'upi_id'  # Replace with the actual UPI ID
+            beneficiary_name = 'beneficiary_name'  # Replace with the actual beneficiary name
+
+            # Simulate a successful payment by returning a JSON response with a payment ID and status
+            response_data = {
+                'booking_id': booking_id,
+                'amount': str(amount),
+                'upi_id': upi_id,
+                'beneficiary_name': beneficiary_name,
+                'payment_id': 'mock_payment_id',  # Replace with a mock payment ID
+                'payment_status': 'success',  # Set the payment status to 'success'
+            }
+
+            # Save payment details to Payment table
+            payment = Payment.objects.create(
+                booking_id=booking_id,
+                amount=str(amount),
+                payment_id=response_data['payment_id'],
+                payment_status=response_data['payment_status'],
+                beneficiary_name=beneficiary_name
+            )
+            job=JobBooking.objects.get(pk=booking_id)
+            job.status= 'Booked'
+            job.save()
+            if job.jobsahayak:
+                if job.jobsahayak.job_type == 'individuals_sahayak':
+                    if job.jobsahayak.count_male == '0' and job.jobsahayak.count_female == '0':
+                        job.jobsahayak.status = 'Booked'
+                        job.jobsahayak.save()
+                else:
+                    job.jobsahayak.status = 'Booked'
+                    job.jobsahayak.save()        
+            else:
+                job.jobmachine.status='Booked'        
+                job.jobmachine.save()
+
+            return Response(response_data)
