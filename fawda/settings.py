0a2394e4a407c3580a38dc45af82a2928d6b9a53
Bablu  Kumar <bablu.kumar@webnyxa.tech>
2023-04-18 19:47:23 +0530

    done debug true

--- fawda/settings.py
+++ fawda/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(h%c9lrv%u%lu)4pr-bqs0x(p49^+jl0^*l5xn$^-il%(7$-lm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
