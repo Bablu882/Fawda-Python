b132d32346fd1d6504965b7918d2bc88f067fd2d
Bablu  Kumar <bablu.kumar@webnyxa.tech>
2023-04-20 15:08:27 +0530

    modified jobs app

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -2,49 +2,89 @@ from rest_framework import serializers
 from .models import *
 
 class PostJobThekePeKamSerializer(serializers.ModelSerializer):
+    land_type = serializers.CharField()
+
+
     class Meta:
-        model=JobSahayak
-        fields=['datetime',
-                'description',
-                'land_area',
-                'land_type',
-                'total_amount_theka'
-                ]
-        
-    def validate(self, data):
-        if not data['datetime'] or not data['description'] or not data['land_area'] or not data['land_type'] or not data['total_amount_theka']:
-            raise serializers.ValidationError({'error': 'All fields are required!'})
-        if not data['land_area'].isdigit():
-            raise serializers.ValidationError({'error': 'Land area should be numeric!'})
-        return data
+        model = JobSahayak
+        fields = ['datetime', 'description', 'land_area', 'land_type', 'total_amount_theka']
+
+    def validate_land_type(self, value):
+        allowed_choices = ['Bigha', 'Killa']
+        if value not in allowed_choices:
+            raise serializers.ValidationError('land_type should be Bigha or Killa')
+        return value
+    
+    def validate_land_area(self, value):
+        try:
+            land_area = int(value)
+        except ValueError:
+            raise serializers.ValidationError('land_area should be a valid integer')
+
+        if land_area <= 0:
+            raise serializers.ValidationError('land_area should be greater than 0')
+        if land_area >= 100:
+            raise serializers.ValidationError('land_area should be less than 100')
+        return land_area
+    
 class GetJobThekePeKamSerializer(serializers.ModelSerializer):
     class Meta:
         model=JobSahayak
         fields='__all__'
-
 class PostJobIndividualSerializer(serializers.ModelSerializer):
+    datetime = serializers.DateTimeField(required=True)
+    description = serializers.CharField(required=True)
+    land_type = serializers.CharField(required=True)
+    land_area = serializers.CharField(required=True)
+    count_male = serializers.CharField(required=True)
+    count_female = serializers.CharField(required=True)
+    pay_amount_male = serializers.CharField(required=True)
+    pay_amount_female = serializers.CharField(required=True)
+    num_days = serializers.CharField(required=True)
+
     class Meta:
-        model=JobSahayak
-        fields=['datetime',
-                'description',
-                'land_area',
-                'land_type',
-                'count_male',
-                'count_female',
-                'pay_amount_male',
-                'pay_amount_female',
-                'num_days',
-                # 'fawda_fee_percentage'
-                # 'total_amount'
-                ]
-   
-          
+        model = JobSahayak
+        fields = ['datetime',
+                  'description',
+                  'land_area',
+                  'land_type',
+                  'count_male',
+                  'count_female',
+                  'pay_amount_male',
+                  'pay_amount_female',
+                  'num_days']
+    def validate_land_type(self, value):
+        allowed_choices = ['Bigha', 'Killa']
+        if value not in allowed_choices:
+            raise serializers.ValidationError('land_type should be Bigha or Killa')
+        return value
+        
+    def validate_land_type(self, value):
+        allowed_choices = ['Bigha', 'Killa']
+        if value not in allowed_choices:
+            raise serializers.ValidationError('land_type should be Bigha or Killa')
+        return value
+    
+    def validate_land_area(self, value):
+        try:
+            land_area = int(value)
+        except ValueError:
+            raise serializers.ValidationError('land_area should be a valid integer')
+
+        if land_area <= 0:
+            raise serializers.ValidationError('land_area should be greater than 0')
+        if land_area >= 100:
+            raise serializers.ValidationError('land_area should be less than 100')
+        return land_area
+
+
 class GetJobIndividualsSerializer(serializers.ModelSerializer):
     class Meta:
         model=JobSahayak
         fields='__all__'        
 
 class JobMachineSerializers(serializers.ModelSerializer):
+    land_type = serializers.CharField()
     others = serializers.CharField(max_length=500, allow_null=True, allow_blank=True)
     class Meta:
         model=JobMachine
@@ -57,6 +97,11 @@ class JobMachineSerializers(serializers.ModelSerializer):
             'land_type',
             'total_amount_machine',    
         ]
+    def validate_land_type(self, value):
+        allowed_choices = ['Bigha', 'Killa']
+        if value not in allowed_choices:
+            raise serializers.ValidationError('land_type should be Bigha or Killa')
+        return value    
     def validate_work_type(self, value):
         if not WorkType.objects.filter(name=value).exists():
             raise serializers.ValidationError("Invalid work type: {}".format(value))
@@ -66,6 +111,19 @@ class JobMachineSerializers(serializers.ModelSerializer):
         if not MachineType.objects.filter(machine=value).exists():
             raise serializers.ValidationError("Invalid machine: {}".format(value))
         return value    
+    
+    def validate_land_area(self, value):
+        try:
+            land_area = int(value)
+        except ValueError:
+            raise serializers.ValidationError('land_area should be a valid integer')
+
+        if land_area <= 0:
+            raise serializers.ValidationError('land_area should be greater than 0')
+        if land_area >= 100:
+            raise serializers.ValidationError('land_area should be less than 100')
+        return land_area
+    
 class GetJobMachineSerializer(serializers.ModelSerializer):
     class Meta:
         model=JobMachine
