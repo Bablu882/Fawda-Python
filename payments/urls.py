09602e7b56c9d1c41bb638bca3adc1954c18ff65
Bablu  Kumar <bablu.kumar@webnyxa.tech>
2023-07-01 14:09:57 +0530

    changed in payment

--- payments/urls.py
+++ payments/urls.py
@@ -2,14 +2,13 @@ from django.urls import path,include
 from .views import *
 from . import views
 from payments.views import payment_show
-# from payments import views
 
 urlpatterns=[
     # path('api/payment/',PaymentAPIView.as_view()),
     path('api/payment_test/',TestPaymentAPIView.as_view()),
     path('api/payment/',PaymentAPIView.as_view()),
     path('api/encrypt_data/',EncryptPaymentParamsView.as_view()),
-    # path('ccavRequestHandler/', views.ccav_request_handler, name='ccav_request_handler'),
+    path('ccavRequestHandlerweb/', views.ccav_request_handler, name='ccav_request_handler'),
     path('ccavResponseHandler/', views.ccav_response_handler, name='ccav_response_handler'),
     path('webprint/',payment_show, name='web_print'),
     path('callback/',res,name='callback'),
